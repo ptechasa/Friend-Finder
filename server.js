d03be93f2fe92bd7cf001ab9042ed2771739498a
miko <ptechasa@mail.ccsf.edu>
2019-10-23 14:11:17 -0700

    add class and unique id for each question

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
-
 var path = require('path');
+var port = process.env.PORT || 3000;
 
 //dotenv package is hide the credential
 require('dotenv').config()
@@ -22,16 +22,28 @@ var connection = mysql.createConnection({
   host: process.env.host,
   user: process.env.user,
   password: process.env.password,
-  database: process.env.database
+  database: process.env.database,
+  // port: 3306
 });
 
 connection.connect();
 
-//GET method route
+// connection.connect(function(err) {
+//   console.log(err.code); // 'ECONNREFUSED'
+//   console.log(err.fatal); // true
+// });
+
+// connection.query('SELECT 1', function (error, results, fields) {
+//   console.log(error.code); // 'ECONNREFUSED'
+//   console.log(error.fatal); // true
+// });
+
+//Use GET method route to main page
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, "/public/home.html"))
 });
 
+//Use GET method route to survey page
 app.get('/survey', function (req, res) {
   res.sendFile(path.join(__dirname, "/public/survey.html"))
 });
@@ -46,7 +58,7 @@ app.get('/api/questions', function (req, res) {
   })
 })
 
-/
+
 app.listen(3000, function () {
   console.log('listening on 3000');
 });
